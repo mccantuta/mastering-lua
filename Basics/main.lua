f6b60269c3359e9d34ce35524f71343424c878e8
Miguel Ccantuta <mccantuta@Miguels-MacBook-Pro.local>
2023-03-09 13:17:55 -0500

    Update

---
+++ Basics/main.lua
@@ -0,0 +1,17 @@
+print("Hello world")
+-- Concatenate two strings
+print("First word" .. "Second word")
+-- String variables
+local name = "Miguel"
+print ("Variable name " .. name)
+
+-- Variable types
+local a = nil
+local b = 15.4
+local c = "Test"
+local d = true
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
---
+++ resources.md
@@ -0,0 +1,2 @@
+# Youtube
+[Full Lua Programming Crash Course - Beginner to Advanced](https://www.youtube.com/watch?v=1srFmjt1Ib0)
